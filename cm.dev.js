c83be15ce5d96da7661daa328a6daeadf611890b
Tim Severien <timseverien@gmail.com>
2013-01-23 15:28:59 +0100

    Added dev file

---
+++ cm.dev.js
@@ -0,0 +1,70 @@
+(function() {
+	function tryParse(obj) {
+		try {
+			return JSON.parse(obj);
+		} catch(e) {};
+		
+		return obj;
+	}
+	
+	function tryStringify(obj) {
+		if(typeof obj !== 'object' || !JSON.stringify) return obj;
+			
+		return JSON.stringify(obj);
+	}
+	
+	window.CM = {
+		set: function(name, value, expires, path, domain) {
+			var pair = escape(name) + '=' + escape(tryStringify(value));
+			
+			if(!!expires) {
+				if(expires.constructor === Number) pair += ';max-age=' + expires;
+				else if(expires.constructor === String) pair += ';expires=' + expires;
+				else if(expires.constructor === Date)  pair += ';expires=' + expires.toUTCString();
+			}
+			
+			pair += ';path=' + ((!!path) ? path : '/');
+			if(!!domain) pair += ';domain=' + domain;
+			
+			document.cookie = pair;
+		},
+		
+		setObject: function(object, expire, path, domain) {
+			for(var key in object) CM.set(key, object[key], expires, path, domain);
+		},
+		
+		get: function(name) {
+			var obj = CM.getObject();
+			
+			return obj[name];
+		},
+	
+		getObject: function() {
+			var pairs = document.cookie.split(/;\s?/i);
+			var object = {};
+			
+			var pair;
+			
+			for(var i in pairs) {
+				pair = pairs[i].split('=');
+				
+				if(pair.length <= 1) continue;
+				
+				object[unescape(pair[0])] = tryParse(unescape(pair[1]));
+			}
+			
+			return object;
+		},
+	
+		unset: function(name) {
+			var date = new Date(0);
+			document.cookie = name + '=; expires=' + date.toUTCString();
+		},
+		
+		clear: function() {
+			var obj = CM.getObject();
+	
+			for(var key in obj) CM.unset(key);
+		}
+	};
+})();
